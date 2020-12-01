ccfec5d8d29ba4351aecb092ec1eeea1ed141ec4
Катя_Кривохижина <krivokhizhina_katya@mail.ru>
2020-12-01 17:00:55 +0500

    color

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace pencils
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = new DateTime(2020, 12, 14, 10, 00, 00);
+            DateTime dateNow = DateTime.Now;
+            Console.WriteLine("До возвразения моих карандашей осталось:");
+            Console.WriteLine(date.Subtract(dateNow));
+        }
+    }
+}
